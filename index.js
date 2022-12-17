918efbbb901bbcce5e380ab4e9e593bdbab1be29
williamsam2000 <106704317+williamsam2000@users.noreply.github.com>
2022-12-17 16:55:45 +0700

    connect mongoose

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const app = express();
 const user = require("./api/user");
 
-// mongoose.set("strictQuery", false);
-// mongoose.connect(process.env.DATABASE_URL);
-// const db = mongoose.connection;
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
 
 // db.on("error", (error) => console.error(error));
 // db.once("open", () => console.log("Connected to database"));
