23c7fe23c7aeb143b05bf1521aa22657b6eec224
williamsam2000 <106704317+williamsam2000@users.noreply.github.com>
2022-12-17 16:43:31 +0700

    hide mongoose and dotenv

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const user = require("./api/user");
 
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
+// mongoose.set("strictQuery", false);
+// mongoose.connect(process.env.DATABASE_URL);
+// const db = mongoose.connection;
 
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to database"));
-app.use(express.json());
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("Connected to database"));
+// app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
