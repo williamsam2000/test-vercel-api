10af79ec3189fda29fa4580f62e0629c9834cc84
williamsam2000 <106704317+williamsam2000@users.noreply.github.com>
2022-12-17 16:58:07 +0700

    add user model

--- model/user.model.js
+++ model/user.model.js
@@ -1,57 +1,57 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const consultWith = new mongoose.Schema({
-//   firstName: {
-//     type: String,
-//     trim: true,
-//   },
-//   lastName: {
-//     type: String,
-//     trim: true,
-//   },
-//   firebaseUID: {
-//     type: String,
-//     trim: true,
-//   },
-// });
+const consultWith = new mongoose.Schema({
+  firstName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+  },
+  firebaseUID: {
+    type: String,
+    trim: true,
+  },
+});
 
-// const User = new mongoose.Schema({
-//   firstName: {
-//     type: String,
-//     trim: true,
-//     // required: "First Name is required",
-//   },
-//   lastName: {
-//     type: String,
-//     trim: true,
-//     // required: "Last Name is required",
-//   },
-//   email: {
-//     type: String,
-//     trim: true,
-//     // required: "Email is required",
-//   },
-//   firebaseUID: {
-//     type: String,
-//     trim: true,
-//   },
-//   role: {
-//     type: String,
-//     trim: true,
-//   },
-//   status: {
-//     type: String,
-//     trim: true,
-//   },
-//   doctorType: {
-//     type: String,
-//     trim: true,
-//   },
-//   imageUrl: {
-//     type: String,
-//     trim: true,
-//   },
-//   consultWith: [consultWith],
-// });
+const User = new mongoose.Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    // required: "First Name is required",
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    // required: "Last Name is required",
+  },
+  email: {
+    type: String,
+    trim: true,
+    // required: "Email is required",
+  },
+  firebaseUID: {
+    type: String,
+    trim: true,
+  },
+  role: {
+    type: String,
+    trim: true,
+  },
+  status: {
+    type: String,
+    trim: true,
+  },
+  doctorType: {
+    type: String,
+    trim: true,
+  },
+  imageUrl: {
+    type: String,
+    trim: true,
+  },
+  consultWith: [consultWith],
+});
 
-// module.exports = mongoose.model("User", User);
+module.exports = mongoose.model("User", User);
