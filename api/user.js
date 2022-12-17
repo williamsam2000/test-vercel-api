d105073022a7b8fe9b4e2c1b672e8550c9980dc2
williamsam2000 <106704317+williamsam2000@users.noreply.github.com>
2022-12-17 17:35:19 +0700

    add routes to user.js
    - add "createUser" method
    - add "getSingleUser" method
    - add "updateUserToDoctor' method

--- api/user.js
+++ api/user.js
@@ -2,24 +2,58 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/user.model");
 
-router.get("/getAllUser", async (req, res) => {
+// add new user
+router.post("/createUser", async (req, res) => {
+  let newUser = req.body.newUser;
+  newUser.role = "customer";
+  newUser.status = "new";
+
+  const user = new User(newUser);
   try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
+    const newUser = await user.save();
+    res.status(200).send(newUser);
+  } catch (err) {
+    res.status(500).json({ message: err.messages });
   }
 });
 
-router.get("/getUser", async (req, res) => {
+// get all user (customer and doctor)
+router.get("/getAllUser", async (req, res) => {
   try {
     const user = await User.find();
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ meesage: err.message });
+  }
+});
+
+// get single user by by firebaseID
+router.get("/getUserByFirebaseID", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      firebaseUID: req.query.firebaseUID,
+    });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+});
+
+// update user role become doctor
+router.post("/userBecomeDoctor", async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      {
+        firebaseUID: req.query.firebaseUID,
+      },
+      {
+        role: "doctor",
+        doctorType: req.query.doctorType,
+      }
+    );
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err });
   }
 });
 
