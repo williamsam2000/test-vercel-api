62fff26c6de29f39d191b2fa3af83f2fd5adb843
williamsam2000 <106704317+williamsam2000@users.noreply.github.com>
2022-12-17 16:28:00 +0700

    test routing

--- api/user.js
+++ api/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/getAllUser", async (req, res) => {
   try {
     res.json({
       status: 200,
