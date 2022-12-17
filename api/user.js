ff3665425bbc353688a39e9e45419cf7b9022fdd
williamsam2000 <106704317+williamsam2000@users.noreply.github.com>
2022-12-17 17:04:12 +0700

    import user model to user routes
    - add getuser method

--- api/user.js
+++ api/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const User = require("../model/user.model");
+const User = require("../model/user.model");
 
 router.get("/getAllUser", async (req, res) => {
   try {
@@ -11,6 +11,15 @@ router.get("/getAllUser", async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
+  }
+});
+
+router.get("/getUser", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ meesage: err.message });
   }
 });
 
